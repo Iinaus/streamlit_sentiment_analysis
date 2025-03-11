702cf3381306cd9885c7e3c62ebd4adc187bfdeb
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2025-03-11 14:28:22 +0200

    Add comments

--- app.py
+++ app.py
@@ -5,39 +5,51 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 
+# Load the training data from a CSV file
 df = pd.read_csv('train.csv', encoding='latin1')
-df.isnull().sum()
+
+# Remove missing values in the dataset
 df.dropna(inplace=True)
 
+# Define the input features (X) and target variable (y)
 x = df['selected_text']
 y = df['sentiment']
+
+# Split the data into training and test sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01, random_state=42)
 
+# Create a machine learning pipeline
 text_clf = Pipeline([
     ('tfidf', TfidfVectorizer()),
     ('clf', LinearSVC())
 ])
 
+# Train the model using the training data
 text_clf.fit(x_train, y_train)
 
 st.write("""
 # Sentiment Analysis
 """)
 
+# Create a text input widget for user input
 st.text_input("Enter a sentence:", key="user_input")
 
 if st.session_state.user_input:
+    # Predict sentiment for the entered sentence
     sentiment = text_clf.predict([st.session_state.user_input])
     sentiment_label = sentiment[0]
 
+    # Define styling options based on sentiment labels
     sentiment_styles = {
         'positive': {'color': 'white', 'background_color': 'green'},
         'negative': {'color': 'white', 'background_color': 'red'},
         'neutral': {'color': 'black', 'background_color': 'yellow'}
     }
 
-    style = sentiment_styles.get(sentiment_label, {'color': 'black', 'background_color': 'gray'}) #default colors if sentiment_label is not found
+    # Get the appropriate style based on the predicted sentiment
+    style = sentiment_styles.get(sentiment_label, {'color': 'black', 'background_color': 'gray'}) # Default style for unknown sentiment
 
+    # Display the sentiment result with dynamic styling in the app
     st.markdown(
         f'<p style="background-color: {style["background_color"]}; color: {style["color"]}; padding: 10px;">'
         f'The sentence was evaluated as {sentiment_label}.</p>',
